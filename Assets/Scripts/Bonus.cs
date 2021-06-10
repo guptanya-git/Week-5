0979cbbef8f008301b5a0eae49db15fc97305ba0
guptanya-git <83054346+guptanya-git@users.noreply.github.com>
2021-06-09 23:40:59 -0500

    Assignment 5.5 Obstacles

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour
+{
+    //counter for the remaining collectibles in the scene
+    public int counter;
+
+    // the prefab that will be instantiated
+    public GameObject bonusObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Pickup()
+    {
+        counter--;
+
+        if (counter == 0)
+            SpawnBonus();
+
+        //Debug.Log("Pickup message has been received.");
+    }
+
+    void SpawnBonus()
+    {
+        //instantiate our prefab
+        GameObject.Instantiate(bonusObject, transform);
+    }
+}
---
+++ Assets/Scripts/RotatorComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorComponent : MonoBehaviour
+{
+    [SerializeField]
+    //the axis around which the gameobject is rotating
+    private Vector3 rotationAxis = Vector3.up;
+    //the speed with which the gameobject is rotating in degrees/frame
+    public float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationAxis, rotationSpeed);   
+    }
+}
